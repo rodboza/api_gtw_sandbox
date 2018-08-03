512937e13b9d16d0068ca642af4353c34deaeca3
rodboza <rodboza@gmail.com>
2018-08-02 23:29:50 -0300

    organizando os arquivos de configuracao e criando o docker compose

---
+++ pocs/install_files/download.sh
@@ -0,0 +1,12 @@
+if !([ -e wso2is-5.6.0.zip ])
+then
+  wget https://www.dropbox.com/s/ruqu2tc9nbce150/wso2is-5.6.0.zip
+fi
+
+if !([ -e wso2am-2.5.0.zip ])
+then
+  wget https://www.dropbox.com/s/7iq7ia5nh52ulri/wso2am-2.5.0.zip
+fi
+
+
+
