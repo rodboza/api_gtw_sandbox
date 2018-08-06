e810a48e62cff079b7d619b5ae3b18aa872fbc58
rodboza <rodboza@gmail.com>
2018-08-05 22:07:54 -0300

    ajustes no script de downloads e criacao do docker-compose v3

--- pocs/install_files/download.sh
+++ pocs/install_files/download.sh
@@ -1,53 +1,39 @@
-if !([ -e wso2is-5.6.0.zip ])
-then
-  echo fazendo download do IS
-  wget -O wso2is-5.6.0.zip https://www.dropbox.com/s/ruqu2tc9nbce150/wso2is-5.6.0.zip?dl=1
-fi
+main()
+{
+  fazer_donwload "wso2is-5.6.0.zip" "https://www.dropbox.com/s/ruqu2tc9nbce150/wso2is-5.6.0.zip?dl=1"
+  fazer_donwload "wso2am-2.5.0.zip" "https://www.dropbox.com/s/7iq7ia5nh52ulri/wso2am-2.5.0.zip?dl=1"
+  fazer_donwload "mysql-connector-java-5.1.46.jar" "https://www.dropbox.com/s/qd5sp7rsy8hn125/mysql-connector-java-5.1.46.jar?dl=1"
+  copiar_arquivo "wso2is-5.6.0.zip" "../wso2is/"
+  copiar_arquivo "wso2am-2.5.0.zip" "../wso2am/"
+  copiar_arquivo "wso2am-2.5.0.zip" "../wso2al/"
+  copiar_arquivo "mysql-connector-java-5.1.46.jar" "../wso2is/"
+  copiar_arquivo "mysql-connector-java-5.1.46.jar" "../wso2am/"
+  copiar_arquivo "mysql-connector-java-5.1.46.jar" "../wso2al/"
+  echo fim
+}
 
-if !([ -e wso2am-2.5.0.zip ])
-then
-  echo fazendo download do AM
-  wget -O wso2am-2.5.0.zip https://www.dropbox.com/s/7iq7ia5nh52ulri/wso2am-2.5.0.zip?dl=1
-fi
+copiar_arquivo()
+{
+  ARQUIVO=$1
+  DESTINO=$2
+  if !([ -e ${DESTINO}${ARQUIVO}])
+  then
+    echo Copiando o arquivo ${ARQUIVO} para ${DESTINO}
+    cp ${ARQUIVO} ${DESTINO}
+  fi
+}
 
-if !([ -e ../wso2is/wso2is-5.6.0.zip ])
-then
-  echo copiando IS
-  cp wso2is-5.6.0.zip ../wso2is/
-fi
-
-if !([ -e ../wso2am/wso2am-2.5.0.zip ])
-then
-  echo copiando AM
-  cp wso2am-2.5.0.zip ../wso2am/
-fi
-
-if !([ -e ../wso2al/wso2am-2.5.0.zip ])
-then
-  echo copiando AL
-  cp wso2am-2.5.0.zip ../wso2al/
-fi
-
-
-if !([ -e ../wso2is/mysql-connector-java-5.1.46.jar ])
-then
-  echo copiando conector mysql para IS
-  cp mysql-connector-java-5.1.46.jar ../wso2is/
-fi
-
-if !([ -e ../wso2am/mysql-connector-java-5.1.46.jar ])
-then
-  echo copiando conector mysql para AM
-  cp mysql-connector-java-5.1.46.jar ../wso2am/
-fi
-
-if !([ -e ../wso2al/mysql-connector-java-5.1.46.jar ])
-then
-  echo copiando conector mysql para AL
-  cp mysql-connector-java-5.1.46.jar ../wso2al/
-fi
+fazer_donwload()
+{
+  ARQUIVO=$1
+  URL=$2
+  if !([ -e ${ARQUIVO} ])
+  then    
+    echo Fazendo download do arquivo ${ARQUIVO}
+    wget -O ${ARQUIVO} ${URL}
+  fi
+}
 
 
-echo fim
 
 
