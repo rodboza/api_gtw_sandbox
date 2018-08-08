90fdb6f7c411d25896fb375462f6c4009beda4b0
rodboza <rodboza@gmail.com>
2018-08-08 00:48:59 -0300

    mudando o mysql para usar imagem propria

--- pocs/build.sh
+++ pocs/build.sh
@@ -2,3 +2,4 @@ docker build -t rbz/wso2al ./wso2al/.
 docker build -t rbz/wso2am ./wso2am/.
 docker build -t rbz/wso2is ./wso2is/.
 docker build -t rbz/httpie ./httpie/.
+docker build -t rbz/mysql ./mysql/.
